da48f9ba976c33de1183b7df1a76e27d465e26e2
Matias Espindola <matiase1993@gmail.com>
2021-11-26 00:59:42 -0300

    Dios, cuantas boludeces, no vuelvo a programar estando empedo

--- pkg/httpHandlers/handleReading.go
+++ pkg/httpHandlers/handleReading.go
@@ -6,7 +6,7 @@ import (
 )
 
 func welcomeHandler() func(w http.ResponseWriter, r *http.Request) {
-	return func(w htto.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode("Hello world!")
 	}
 }
