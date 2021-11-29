03f6035fdb84b4de698f66f337f75f87d1d3be14
Matias Espindola <matiase1993@gmail.com>
2021-11-29 00:49:34 -0300

    Agregado templates

--- cmd/main.go
+++ cmd/main.go
@@ -3,17 +3,35 @@ package main
 import (
 	"io"
 	"log"
+	"html/template"
 	"net/http"
 	"os"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World!")
+var view *template.Template
+
+type pageData struct {
+	Title string
+	Firstname string
+}
+
+func init() {
+	view = template.Must(template.ParseGlob("template/*.gohtml"))
 }
 
 func main() {
 	port := os.Getenv("PORT")
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", indexPage)
 	log.Print("Listening on port " + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func indexPage(w http.ResponseWriter, r *http.Request) {
+	pd := pageData {Title:"Pagina Indice"}
+
+	err := view.ExecuteTemplate(w, "index.gohtml", pd)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
 }
